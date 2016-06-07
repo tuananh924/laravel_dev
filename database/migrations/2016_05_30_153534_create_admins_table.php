c281057aa8b0039340995a466482189422aafc88
tuananh <tuananhcnttuneti@gmail.com>
2016-06-07 21:32:30 +0700

    Task//Create migrations

---
+++ database/migrations/2016_05_30_153534_create_admins_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdminsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+       Schema::create('tbl_admins', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username', 50);
+            $table->string('password', 100);
+            $table->string('email', 150)->unique();
+            $table->tinyInteger('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
---
+++ database/migrations/2016_05_30_153908_create_user_courses_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+       Schema::create('tbl_user_courses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('course_id');
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
